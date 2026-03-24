776d643dcaf4ba30171b0c1d4118d60ed819d9c6
levigss <gutierrezsantiagojonathanlevigen2024@cbtis116.edu.mx>
2026-03-23 18:08:56 -0700

    brbrbbrb

--- dj_blog/blog_main/urls.py
+++ dj_blog/blog_main/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from blog.views import home
 
 urlpatterns = [
     path('home/', home, name='home'),
+    path('blog/', include('blog.urls'))
 ]
