a9799fa65ce995e928e81376968c3b57ce4ed0e4
levigss <gutierrezsantiagojonathanlevigen2024@cbtis116.edu.mx>
2026-03-25 16:15:32 -0700

    brrr

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,25 @@
 from django.db import models
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
-
     class Meta:
-        verbose_name_plural = "Categories" # Así corriges el nombre en el admin
-# Create your models here.
+        verbose_name_plural = "Categories"
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+
+# AGREGA ESTO:
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author}'
