a5b38cbd79a4f38485c6420e76fb6ac2480b8ad0
levigss <gutierrezsantiagojonathanlevigen2024@cbtis116.edu.mx>
2026-03-26 17:09:43 -0700

    brrrrrrrrrrrrrrr

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 # Estaba incompleto
+from .models import Post
 
-# Create your views here.
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/home.html', {'posts': posts})
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/detail.html', {'post': post})
